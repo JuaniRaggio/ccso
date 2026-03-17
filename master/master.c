c9e95a66687d69beb6fd4ec38a4fc3c5b7321a18
scibeira <142165670+scibeira@users.noreply.github.com>
2026-03-17 18:06:43 -0300

    I've added a function to initialize the board with numbers from 1 to 9. I've also added a print function so we can see if we've actually initialized the board.

--- master/master.c
+++ master/master.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <time.h>
 
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 #include "../common/include/game_state.h"
+#include "master.h"
 
 #define PERMISSIONS 0666
 #define MAPFLAG MAP_SHARED
 #define OPENFLAGS O_CREAT | O_RDWR
 #define PROTECCTIONS PROT_READ | PROT_WRITE
+
+void printBoard(int8_t board[], uint16_t height, uint16_t width);
 
 int main(int argc, char* argv[]){
 
@@ -17,11 +21,14 @@ int main(int argc, char* argv[]){
    We need a function that Store the parameters that the user entered via STDIN. 
    First we save them (localVars or struct), then  we create sharedMemory, after that we save them in the sharedMemory struct
    */
-   int height, width; // This should not be here, we need a function that complete task 1).
 
+   int height = 3, width = 4; // This should not be here
    game_state_t * sharedMemory = NULL;
-   sharedMemory = ptrcreateSharedMemory("/game_state", (sizeof(game_state_t) + (sizeof(int8_t) * height *width)), OPENFLAGS, PERMISSIONS, PROTECCTIONS, MAPFLAG);
+   sharedMemory = createSharedMemory("/game_state", (sizeof(game_state_t) + (sizeof(int8_t) * height *width)), OPENFLAGS, PERMISSIONS, PROTECCTIONS, MAPFLAG);
    // Con ese puntero accedemos a la memoria compartida dsd cualquier proceso posterior al fork()
+
+   initializeBoard(sharedMemory->board, height, width);
+   printBoard(sharedMemory->board, height, width); //Para ver que se guardo el board en la shared memory
 }
 
 
@@ -54,4 +61,30 @@ game_state_t * createSharedMemory(char *sharedMemoryName, int totalSize, int ope
    }
    close(fd);
    return ptr;
+}
+
+void initializeBoard(int8_t board[], uint16_t height, uint16_t width){
+
+   srand(time(NULL));
+   for(int i = 0; i < height; i++){
+      for(int j = 0; j < width; j++){
+         board[ 4*i + j ] = random_1_9();
+      }
+   }
+   return;
+}
+
+void printBoard(int8_t board[], uint16_t height, uint16_t width){
+    
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            printf("%d ",board[ 4*i + j] ); //De izq a der y de arriba a abajo (en ese orden)
+        }
+        printf("\n");
+    }
+    return;
+}
+   
+int random_1_9() {
+   return (rand() % 9) + 1;
 }
