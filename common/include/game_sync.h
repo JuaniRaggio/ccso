df2e5abac17aa235c8a35430894a11ff3fb0a7dc
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-06 20:03:25 -0300

    fix: names

--- common/include/game_sync.h
+++ common/include/game_sync.h
@@ -1,15 +1,17 @@
 #pragma once
 
+#include "game_state.h"
 #include <semaphore.h>
+#include <stdint.h>
 
 const char *const game_sync_memory_name = "/game_sync";
 
 typedef struct {
-   sem_t pending_changes_mutex; // El máster le indica a la vista que hay cambios por imprimir
-   sem_t printing_mutex;        // La vista le indica al máster que terminó de imprimir
-   sem_t master_mutex;          // Mutex para evitar inanición del máster al acceder al estado
+   sem_t pending_changes_to_show; // El máster le indica a la vista que hay cambios por imprimir
+   sem_t printing;        // La vista le indica al máster que terminó de imprimir
+   sem_t master_writing;          // Mutex para evitar inanición del máster al acceder al estado
    sem_t gamestate_mutex;
    sem_t readers_count_mutex;
-   unsigned int readers_count; // Cantidad de jugadores leyendo el estado
-   sem_t may_send_movement[9];
+   uint32_t readers_count; // Cantidad de jugadores leyendo el estado
+   sem_t player_may_send_movement[MAX_PLAYERS];
 } game_sync_t;
