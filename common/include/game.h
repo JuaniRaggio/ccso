210ba7f52149332ab5fe4fae2344eb7ff84ed846
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-06 20:13:26 -0300

    feat: game cdt

---
+++ common/include/game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef struct game_cdt_t * game_t;
+
+game_t game_init(char * argv[], int argc);
+game_t game_connect(uint32_t w, uint32_t h);
+void game_disconnect(game_t);
+void game_end(game_t);
+
