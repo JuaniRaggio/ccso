93b156832bafdaee48a0190c60bf8dac7bda68d5
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-10 00:01:42 -0300

    feat(view): main structure

--- view/main.c
+++ view/main.c
@@ -1,6 +1,28 @@
+#include "view.h"
+#include <game.h>
+#include <game_state.h>
+#include <game_sync.h>
+#include <ncurses.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("View process started\n");
+    game_t game = new_game(view);
+
+
+    view_t view;
+    view_init(&view);
+
+    while (!should_exit && game.state->running) {
+        if (should_exit || !game.state->running) {
+            break;
+        }
+    }
+
+    view_cleanup(&view);
+    game_disconnect(&game);
+
     return 0;
 }
