61c9542219d3cb17b186b61d71b9c9ce2487c5b9
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-10 19:00:11 -0300

    fix(common): comments

--- common/include/game_sync.h
+++ common/include/game_sync.h
@@ -1,5 +1,11 @@
 #pragma once
 
+/**
+ * @brief: This is the sync's file for this project, it is placed in
+ *         common/ just because the contract has to be simetric such
+ *         as in any communication protocol
+ **/
+
 #include "game_state.h"
 #include <semaphore.h>
 #include <stdint.h>
@@ -11,12 +17,12 @@ static const uint32_t SEM_UNLOCKED = 1;
 const char *const game_sync_memory_name = "/game_sync";
 
 typedef struct {
-    sem_t pending_changes_to_show; // El máster le indica a la vista que hay cambios por imprimir
-    sem_t printing;                // La vista le indica al máster que terminó de imprimir
-    sem_t master_writing;          // Mutex para evitar inanición del máster al acceder al estado
+    sem_t pending_changes_to_show;
+    sem_t printing;
+    sem_t master_writing;
     sem_t gamestate_mutex;
     sem_t readers_count_mutex;
-    uint32_t readers_count; // Cantidad de jugadores leyendo el estado
+    uint32_t readers_count;
     sem_t player_may_send_movement[MAX_PLAYERS];
 } game_sync_t;
 
