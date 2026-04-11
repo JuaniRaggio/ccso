c179125e7aafc0a7397b8096aeda91ba04973a98
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-11 16:12:31 -0300

    fix: game state init numbers

--- master/src/game_init.c
+++ master/src/game_init.c
@@ -9,17 +9,17 @@
 #define MIN_CELL_REWARD 1
 #define REWARD_RANGE (MAX_CELL_REWARD - MIN_CELL_REWARD + 1)
 
-static void init_board(game_state_t *state, uint16_t width, uint16_t height) {
+static void board_init(game_state_t *state, uint16_t width, uint16_t height, int8_t players) {
     state->width = width;
     state->height = height;
-    state->players_count = 0;
+    state->players_count = players;
     state->running = false;
     for (uint32_t i = 0; i < (uint32_t)height * width; i++) {
         state->board[i] = (random() % REWARD_RANGE) + MIN_CELL_REWARD;
     }
 }
 
-void game_init(game_t *game, uint16_t width, uint16_t height, uint64_t seed) {
+void game_state_init(game_t *game, uint16_t width, uint16_t height, uint64_t seed, int8_t players) {
     srandom(seed);
-    init_board(game->state, width, height);
+    board_init(game->state, width, height, players);
 }
