abc19826ecca1fce4ffac218d7233b8eae65bd51
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-11 16:59:18 -0300

    fix(sync): sem destroyer

--- common/src/game_sync.c
+++ common/src/game_sync.c
@@ -1,5 +1,6 @@
 #include <game_sync.h>
 #include <semaphore.h>
+#include <sys/semaphore.h>
 
 void game_sync_writer_enter(game_sync_t *sync) {
     sem_wait(&sync->master_writing);
@@ -54,7 +55,6 @@ void game_sync_player_wait_turn(game_sync_t *sync, uint8_t player_idx) {
 }
 
 void game_sync_player_grant_turn(game_sync_t *sync, uint8_t player_idx) {
-
     sem_post(&sync->player_may_send_movement[player_idx]);
 }
 
@@ -71,13 +71,13 @@ void game_sync_init(game_sync_t *sync) {
 }
 
 void game_sync_destroy(game_sync_t *sync) {
-    sem_init(&sync->view_may_render, SEM_SHARED_BETWEEN_PROCESSES, SEM_LOCKED);
-    sem_init(&sync->view_rendered, SEM_SHARED_BETWEEN_PROCESSES, SEM_LOCKED);
-    sem_init(&sync->master_writing, SEM_SHARED_BETWEEN_PROCESSES, SEM_UNLOCKED);
-    sem_init(&sync->gamestate_mutex, SEM_SHARED_BETWEEN_PROCESSES, SEM_UNLOCKED);
-    sem_init(&sync->readers_count_mutex, SEM_SHARED_BETWEEN_PROCESSES, SEM_UNLOCKED);
+    sem_destroy(&sync->view_may_render);
+    sem_destroy(&sync->view_rendered);
+    sem_destroy(&sync->master_writing);
+    sem_destroy(&sync->gamestate_mutex);
+    sem_destroy(&sync->readers_count_mutex);
     sync->readers_count = 0;
     for (uint8_t i = 0; i < MAX_PLAYERS; i++) {
-        sem_init(&sync->player_may_send_movement[i], SEM_SHARED_BETWEEN_PROCESSES, SEM_UNLOCKED);
+        sem_destroy(&sync->player_may_send_movement[i]);
     }
 }
