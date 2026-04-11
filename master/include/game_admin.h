a56bc658be8adba96fce872957f932dbe6dfd346
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-11 17:57:19 -0300

    feat(game_admin): player registration

--- master/include/game_admin.h
+++ master/include/game_admin.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#include "game_state.h"
 #include <game.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <sys/_types/_pid_t.h>
 
 void game_state_init(game_t *game, uint16_t width, uint16_t height, uint64_t seed, int8_t players);
+
+bool game_register_player(game_t * game, size_t idx, pid_t pid, const char name[MAX_NAME_LENGTH]);
--- master/src/game_admin.c
+++ master/src/game_admin.c
@@ -1,9 +1,12 @@
+#include "error_management.h"
 #include <game_admin.h>
 #include <game_state.h>
 #include <game_sync.h>
 #include <semaphore.h>
+#include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_CELL_REWARD 9
 #define MIN_CELL_REWARD 1
@@ -22,4 +25,21 @@ static void board_init(game_state_t *state, uint16_t width, uint16_t height, int
 void game_state_init(game_t *game, uint16_t width, uint16_t height, uint64_t seed, int8_t players) {
     srandom(seed);
     board_init(game->state, width, height, players);
+}
+
+bool game_register_player(game_t *game, size_t idx, pid_t pid, const char name[MAX_NAME_LENGTH]) {
+    // If player at index already exists, we override it
+    if (idx >= MAX_PLAYERS || name == NULL || name[0] == '\0') {
+        manage_error(__FILE__, __func__, __LINE__, invalid_argument_error);
+        return false;
+    }
+    game->state->players[idx] = (player_t){
+        .player_id = pid,
+        .state = true,
+        .invalid_moves = 0,
+        .valid_moves = 0,
+    };
+    strncpy(game->state->players[idx].name, name, MAX_NAME_LENGTH - 1);
+    game->state->players[idx].name[MAX_NAME_LENGTH - 1] = '\0'; // JIC
+    return true;
 }
