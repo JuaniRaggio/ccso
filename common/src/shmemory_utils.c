f83efae84311eb8bcb64f0dab83a70362be8b6e4
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-11 19:35:11 -0300

    fix(shm): usage of new stack-trace error tracking

--- common/src/shmemory_utils.c
+++ common/src/shmemory_utils.c
@@ -10,24 +10,23 @@ static inline bool is_creator(int open_flags) {
     return open_flags & O_CREAT;
 }
 
-void *_create_shm(const shm_data_t *data, size_t shm_size, error_manager_t manage_error, const char *file,
-                  const char *func, uint64_t line) {
+void *_create_shm(const shm_data_t *data, size_t shm_size, error_manager_t manage_error, trace_t caller) {
     int fd = shm_open(data->shared_memory_name, data->open_flags, data->permissions);
     if (fd == -1) {
-        manage_error(file, func, line, errno);
+        manage_error(HERE, caller, errno);
         return NULL;
     }
 
     // O_CREAT means that you are the creator of a new shared memory object
     if (is_creator(data->open_flags) && ftruncate(fd, shm_size) == -1) {
-        manage_error(file, func, line, errno);
+        manage_error(HERE, caller, errno);
         close(fd);
         return NULL;
     }
 
     void *ptr = mmap(NULL, shm_size, data->protections, data->map_flag, fd, data->offset);
     if (ptr == MAP_FAILED) {
-        manage_error(file, func, line, mapping_error);
+        manage_error(HERE, caller, mapping_error);
         close(fd);
         return NULL;
     }
@@ -35,13 +34,12 @@ void *_create_shm(const shm_data_t *data, size_t shm_size, error_manager_t manag
     return ptr;
 }
 
-void _destroy_shm(void *ptr, size_t shm_size, error_manager_t manage_error, const char *file, const char *func,
-                  uint64_t line) {
+void _destroy_shm(void *ptr, size_t shm_size, error_manager_t manage_error, trace_t caller) {
     if (ptr == NULL) {
-        manage_error(file, func, line, invalid_argument_error);
+        manage_error(HERE, caller, invalid_argument_error);
         return;
     }
     if (munmap(ptr, shm_size) == -1) {
-        manage_error(file, func, line, errno);
+        manage_error(HERE, caller, errno);
     }
 }
