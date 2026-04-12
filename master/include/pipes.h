58adc7f443280ef795fe0992e2a818d84f5ea4aa
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-11 21:44:31 -0300

    fix: pipe hardcoding

--- master/include/pipes.h
+++ master/include/pipes.h
@@ -6,13 +6,17 @@
 #include <sys/select.h>
 #include "game_state.h"
 
-#define READ 0
-#define WRITE 1
 
-void createPipes(int pipes[][2], int playersCount);
+typedef enum {
+    pipe_reader = 0,
+    pipe_writer,
+    pipe_ends,
+} pipe_users_t;
 
-void forkPlayers(int pipes[][2], int playersCount, game_state_t *game_state);
+void createPipes(int pipes[][pipe_ends], int playersCount);
 
-void closePipes(int pipes[][2], int action, int playersCount);
+void forkPlayers(int pipes[][pipe_ends], int playersCount, game_state_t *game_state);
 
-void initFdSet(fd_set *masterSet, int pipes[][2], int playersCount, int *maxFd);
+void closePipes(int pipes[][pipe_ends], int action, int playersCount);
+
+void initFdSet(fd_set *masterSet, int pipes[][pipe_ends], int playersCount, int *maxFd);
