15768cd4423f68f63508fcc04e9daf772e67b38b
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-12 14:36:45 -0300

    feat: move_deltas

---
+++ common/src/player_protocol.c
@@ -0,0 +1,12 @@
+#include "player_protocol.h"
+
+const move_delta_t move_delta[dir_count] = {
+    [dir_up]         = { 0, -1},
+    [dir_up_right]   = { 1, -1},
+    [dir_right]      = { 1,  0},
+    [dir_down_right] = { 1,  1},
+    [dir_down]       = { 0,  1},
+    [dir_down_left]  = {-1,  1},
+    [dir_left]       = {-1,  0},
+    [dir_up_left]    = {-1, -1},
+};
