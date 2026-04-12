74e888deda8e02d72280a90d1376354d6427a265
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-12 20:53:21 -0300

    fix: magic pipe_ends are contemplated

--- master/include/game_admin.h
+++ master/include/game_admin.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "game_state.h"
+#include "pipes.h"
 #include "player_protocol.h"
 #include <game.h>
 #include <stddef.h>
@@ -35,7 +36,7 @@ void register_move(game_state_t *state, bool is_valid_move, int8_t playerId);
 
 bool process_player_move(game_state_t *state, uint8_t player_idx, direction_wire_t direction);
 
-bool handle_player_turn(game_t *game, int32_t pipes[][2], fd_set *readFds, fd_set *masterSet, int8_t idx,
+bool handle_player_turn(game_t *game, int32_t pipes[][pipe_ends], fd_set *readFds, fd_set *masterSet, int8_t idx,
                         bool *out_valid);
 
 void register_players_from_paths(game_state_t *state, const char *paths[]);
@@ -44,6 +45,6 @@ bool any_player_alive(game_state_t *state);
 
 void place_players_on_board(game_state_t *state);
 
-round_result_t process_round(game_t *game, int32_t pipes[][2], fd_set *readFds, fd_set *masterSet, int8_t start_player);
+round_result_t process_round(game_t *game, int32_t pipes[][pipe_ends], fd_set *readFds, fd_set *masterSet, int8_t start_player);
 
 void print_game_results(game_state_t *state);
