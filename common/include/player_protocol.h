14e75a1d2cfad3e05b597707fdc809ea050e30d7
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-12 14:36:26 -0300

    feat: protocol inline functions

--- common/include/player_protocol.h
+++ common/include/player_protocol.h
@@ -25,3 +25,11 @@ typedef struct {
 
 extern const move_delta_t move_delta[dir_count];
 
+static inline bool is_valid_direction(direction_wire_t d) {
+    return d >= 0 && d < dir_count;
+}
+
+static inline void apply_direction(int16_t x, int16_t y, direction_t dir, int16_t *out_x, int16_t *out_y) {
+    *out_x = x + move_delta[dir].dx;
+    *out_y = y + move_delta[dir].dy;
+}
