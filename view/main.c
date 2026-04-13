9d18974ae1295928b2dea66b1a166df8156ad469
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-12 23:03:13 -0300

    fix: view code restructure

--- view/main.c
+++ view/main.c
@@ -19,12 +19,53 @@ static void signal_handler(int32_t sig) {
     should_exit = 1;
 }
 
+static void setup_signals() {
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+}
+
+static void initialize_game_view(view_t *view, uint16_t width, uint16_t height) {
+    if (getenv("TERM") == NULL) {
+        setenv("TERM", "xterm-256color", 0);
+    }
+    view_init(view, width, height);
+}
+
+static void handle_resize(view_t *view, uint16_t width, uint16_t height) {
+    view_cleanup(view);
+    view_init(view, width, height);
+}
+
+static void run_game_loop(view_t *view, game_t *game, uint16_t width, uint16_t height) {
+    while (1) {
+        game_sync_view_wait_frame(game->sync);
+        if (should_exit || !game->state->running) {
+            game_sync_view_frame_done(game->sync);
+            break;
+        }
+
+        if (getch() == KEY_RESIZE) {
+            handle_resize(view, width, height);
+        }
+
+        view_draw_all(view, game->state);
+        game_sync_view_frame_done(game->sync);
+    }
+}
+
+static void show_final_results(view_t *view, game_state_t *state) {
+    if (should_exit) return;
+    view_draw_all(view, state);
+    view_draw_endscreen(view, state);
+    nodelay(stdscr, FALSE);
+    getch();
+}
+
 int main(int argc, char *argv[]) {
     if (!setlocale(LC_ALL, "C.utf8")) {
         setlocale(LC_ALL, "");
     }
-    signal(SIGINT, signal_handler);
-    signal(SIGTERM, signal_handler);
+    setup_signals();
 
     uint16_t width, height;
     if (!parse_board_args(argv, &width, &height)) {
@@ -32,36 +73,11 @@ int main(int argc, char *argv[]) {
     }
 
     game_t game = new_game(view, .height = height, .width = width);
-
-    if (getenv("TERM") == NULL) {
-        setenv("TERM", "xterm-256color", 0);
-    }
-
     view_t game_view;
-    view_init(&game_view, width, height);
+    initialize_game_view(&game_view, width, height);
 
-    while (1) {
-        game_sync_view_wait_frame(game.sync);
-        if (should_exit || !game.state->running) {
-            game_sync_view_frame_done(game.sync);
-            break;
-        }
-        int32_t ch = getch();
-        if (ch == KEY_RESIZE) {
-            view_cleanup(&game_view);
-            view_init(&game_view, width, height);
-        }
-
-        view_draw_all(&game_view, game.state);
-        game_sync_view_frame_done(game.sync);
-    }
-
-    if (!should_exit) {
-        view_draw_all(&game_view, game.state);
-        view_draw_endscreen(&game_view, game.state);
-        nodelay(stdscr, FALSE);
-        getch();
-    }
+    run_game_loop(&game_view, &game, width, height);
+    show_final_results(&game_view, game.state);
 
     view_cleanup(&game_view);
     game_disconnect(&game);
